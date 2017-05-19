2e414aaaced4ed6ab7442bd9412c9bf252d38bf3
Vincent Lai <vincent.lai@trubuzz.com>
2017-05-19 16:01:42 +0800

    LeetCode1 Finished

---
+++ LeetCodeUnitTest/LeetCode1TwoSumUnitTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+using System.Linq;
+
+namespace LeetCodeUnitTest
+{
+    [TestClass]
+    public class LeetCode1TwoSumUnitTest
+    {
+        [TestMethod]
+        public void Input_1_8_Target_9()
+        {
+            var num = new int[] { 1, 8 };
+            var target = 9;
+            var expect = new int[] { 0, 1 };
+            
+            var actual = TwoSum(num, target);
+            ShouldEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_1_8_3_7Target_9()
+        {
+            var num = new int[] { 1, 8, 3, 7 };
+            var target = 9;
+            var expect = new int[] { 0, 1 };
+            
+            var actual = TwoSum(num, target);
+            ShouldEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_1_2_4_7Target_6()
+        {
+            var num = new int[] { 1, 2, 4, 7 };
+            var target = 6;
+            var expect = new int[] { 1, 2 };
+
+            var actual = TwoSum(num, target);
+            ShouldEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_3_8_5_2Target10()
+        {
+
+            var num = new int[] { 3, 8, 5, 2 };
+            var target = 10;
+            var expect = new int[] { 1, 3 };
+
+            var actual = TwoSum(num, target);
+            ShouldEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_1_3_5_7_9_2_4_6_8Target17()
+        {
+
+            var num = new int[] { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
+            var target = 17;
+            var expect = new int[] { 4, 8 };
+
+            var actual = TwoSum(num, target);
+            ShouldEqual(expect, actual);
+        }
+
+        private static int[] TwoSum(int[] num, int target)
+        {
+            var sol = new Solution();
+            return sol.TwoSum(num, target);
+        }
+
+        private static void ShouldEqual(int[] expect, int[] actual)
+        {
+            CollectionAssert.AreEqual(expect.OrderBy(x => x).ToArray(), actual.OrderBy(x => x).ToArray(),);
+            
+        }
+
+    }
+}
--- Solutions/Solution.cs
+++ Solutions/Solution.cs
@@ -10,7 +10,23 @@ namespace Solutions
     {
         public int[] TwoSum(int[] nums, int target)
         {
+            var sortedDic = nums.Select((x, i) => new Tuple<int, int>(x, i)).OrderBy(x => x.Item1)
+                .Select((x, i) => new { element = x, index = i }).ToDictionary(k => k.index, v => v.element);
 
+            var start = 0;
+            var end   = nums.Count()-1;
+
+            for(;start<end;)
+            {
+                if (sortedDic[start].Item1 + sortedDic[end].Item1 == target)
+                    return new int[] { sortedDic[start].Item2, sortedDic[end].Item2 };
+                if (sortedDic[start].Item1 + sortedDic[end].Item1 > target)
+                    --end;
+                else
+                    ++start;
+            }
+            
+            return new int[] { -1, -1 };
         }
     }
 }
